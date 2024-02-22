cf2f2179f3718422f174e5c06c4c485b6dbe8bc8
vishal suthar <vishal123suthar2@gmail.com>
2024-02-23 02:30:08 +0530

    insertion Sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,37 +1,29 @@
 public class SelectionSort {
-    public static void main(String args[]){
 
-        int[] nums = {6,5,2,1,3,9};
-        int size = nums.length;
-        int temp;
-        int minindex = -1;
-
-        for(int num : nums){        //diplay the array using foreach loop
-             System.out.print(num);
-        }
-
-    
-
-    for(int i = 0;i<size - 1; i++){
-        minindex = i;
-
-        for(int j = 0 + 1; j < size; j++){
-            if(nums[minindex] > nums[j]){
-               minindex = j; 
-            }
-
-            System.out.print( " ");
-
-       temp  = nums[minindex];
-       nums[minindex] = nums[i];
-       nums[i] =  temp;
-
-        for(int num : nums){        //diplay the array using foreach loop
-            System.out.print(num);
-       }
-
+    public static void printarray(int arr[]){
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i] + " ,");
         }
     }
+    public static void main(String args[]){
+ 
+        int arr[] = {7,8,3,1,2};
 
-}
-}
+        for(int i = 0;i<arr.length-1;i++){
+             int smallest = i;
+            for(int j =i + 1; j<arr.length-i-1; j++){
+                 
+                if(arr[smallest] > arr[j]){
+                   smallest = j;
+                }
+
+                }
+
+                int temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+            printarray(arr);
+        }
+    
+    }
